4b13d93f8a197b2436f886a5d99e239bdc8e1d8a
sina <sina.keshmiri@gmail.com>
2018-10-16 13:38:41 -0700

    public added

--- lab5/server.js
+++ lab5/server.js
@@ -5,13 +5,31 @@ const port = porocess.env.PORT || 8080
 
 app.set('view engine', 'ejs')
 app.use(bodyPasrer.urlencoded({extended: false}))
-aapp.use(express.static('public'))
+app.use(express.static('public'))
 
 app.get('/', (req,res) => {
-
+    
 })
 
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`)
 })
+let blogs = [
+    {
+      id: 'blog-title-1',
+      title: 'Blog Title 1',
+      summary: 'This is a blog summary.',
+      content: 'This is the blog content',
+      author: 'John Smith',
+      created: 'February 5, 2018'
+    },
+    {
+      id: 'blog-title-2',
+      title: 'Blog Title 2',
+      summary: 'This is a blog summary.',
+      content: 'This is the blog content',
+      author: 'John Smith',
+      created: 'February 6, 2018'
+    },
+  ]
