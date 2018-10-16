4acb66fd7b38cdc4f87358a1cf92a0d048dea782
sina <sina.keshmiri@gmail.com>
2018-10-16 13:30:48 -0700

    files added

---
+++ lab5/server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const bodyPasrer = require('body-parser')
+
+const port = porocess.env.PORT || 8080 
+
+app.set('view engine', 'ejs')
+app.use(bodyPasrer.urlencoded({extended: false}))
+aapp.use(express.static('public'))
+
+app.get('/', (req,res) => {
+    res.
+})
+
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`)
+})
