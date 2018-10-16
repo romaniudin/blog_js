de627b9825dc46f455461441113ccdad6e031fb4
sina <sina.keshmiri@gmail.com>
2018-10-16 14:46:24 -0700

    added 2

--- lab5/server.js
+++ lab5/server.js
@@ -10,7 +10,27 @@ app.use(express.static('public'))
 app.get('/', (req,res) => {
     res.render('index', {blogs})
 })
-
+app.get('/blog/:id', (req, res) => {
+    const { id } = req.params
+    blogs.forEach(blog => {
+        if (id === blog.id) {
+              res.render('blogPost', {blog})
+        }
+    })
+})
+app.post('/addpost', (req, res) => {
+   const author = req.body.author
+   const content = req.body.content
+//    const { author, content } = req.body
+   blogs.push({
+    id: 'blog-title-4',
+    title: 'Blog Title 1',
+    summary: 'This is a blog summary.',
+    content,
+    author,
+    created: 'February 5, 2018'
+   })
+})
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`)
@@ -20,16 +40,16 @@ let blogs = [
       id: 'blog-title-1',
       title: 'Blog Title 1',
       summary: 'This is a blog summary.',
-      content: 'This is the blog content',
-      author: 'John Smith',
+      content: `i'm at brainstation`,
+      author: 'Roman',
       created: 'February 5, 2018'
     },
     {
       id: 'blog-title-2',
       title: 'Blog Title 2',
       summary: 'This is a blog summary.',
-      content: 'This is the blog content',
-      author: 'John Smith',
+      content:  `i'm at brainstion too`,
+      author: 'Sina',
       created: 'February 6, 2018'
     },
   ]
